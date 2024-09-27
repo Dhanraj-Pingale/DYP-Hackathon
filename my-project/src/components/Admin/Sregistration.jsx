b8b2dd344dbc4a6c084a21c1855a771fe1776599
YashBarbole <yashbarbole7110@gmail.com>
2024-09-27 15:38:19 +0530

    register login  pages

--- my-project/src/components/Admin/Sregistration.jsx
+++ my-project/src/components/Admin/Sregistration.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import student from "../../Assets/student.jpg"
 
 const Sregistration = () => {
   const [studentName, setName] = useState("");
@@ -28,9 +29,10 @@ const Sregistration = () => {
  
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-md w-80">
-        <h2 className="text-2xl font-semibold mb-4 text-center">Register</h2>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 absolute inset-0 bg-cover bg-center" style={{ backgroundImage: `url(${student})` }}>
+             <div className="absolute z-40 inset-0 bg-black opacity-50" />
+      <div className="bg-white p-8 z-50  rounded-lg shadow-md w-80">
+        <h2 className="text-3xl font-semibold mb-4 text-center text-zinc-600"> Student Register</h2>
         
         {error && <p className="text-red-500 text-center">{error}</p>}
 
@@ -40,7 +42,7 @@ const Sregistration = () => {
               type="text"
               name="name"
               placeholder="Name of Student"
-              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+              className="w-full p-2 border bg-white border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
               autoComplete="off"
               required
               onChange={(e) => setName(e.target.value)}
@@ -51,7 +53,7 @@ const Sregistration = () => {
               type="number"
               name="studentID"
               placeholder="Create ID"
-              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+              className="w-full p-2 border bg-white border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
               autoComplete="off"
               required
               onChange={(e) => setStudentID(e.target.value)}
@@ -62,7 +64,7 @@ const Sregistration = () => {
               type="password"
               name="password"
               placeholder="Set Password"
-              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
+              className="w-full p-2 border bg-white border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
               autoComplete="off"
               required
               onChange={(e) => setPassword(e.target.value)}
@@ -70,7 +72,7 @@ const Sregistration = () => {
           </div>
           <button
             type="submit"
-            className="w-full p-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-200"
+            className="w-full p-2 bg-[#00ACD6] text-white rounded-md hover:bg-[#329db7]transition duration-200"
           >
             Add Student
           </button>
