eb2434f4ee81d06bf07a4a21531ca18dbb424fc7
tusharbhoge <tusharbhoge80@gmil.com>
2024-09-27 19:19:10 +0530

    notice card added

--- my-project/src/components/student/Sdashboard.jsx
+++ my-project/src/components/student/Sdashboard.jsx
@@ -90,8 +90,8 @@ const Sdashboard = () => {
                   key={notice._id}
                   className="flex flex-col items-center bg-orange-200 w-[300px] h-auto text-gray-900 p-8 rounded-xl shadow-lg transform transition-transform duration-200 hover:scale-105"
                 >
-                  <h2 className="text-2xl font-bold mb-4 text-gray-800">{notice.title}</h2>
-                  <p className="text-lg text-gray-600">{notice.description}</p>
+                  <h2 className="text-2xl font-bold mb-4 text-gray-800">{notice.subject}</h2>
+                  <p className="text-lg text-gray-600">{notice.noticeDescription}</p>
                 </div>
               ))}
             </div>
